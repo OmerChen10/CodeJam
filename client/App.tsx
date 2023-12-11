2118a38312dbdd76b0dd7cbd6ef11450bbd4b422
OmerChen10 <o.c33211@gmail.com>
2023-12-12 01:45:27 +0200

    Removed unused code

--- client/App.tsx
+++ client/App.tsx
@@ -1,10 +1,7 @@
-import { useRef } from "react"
-import { NetworkManager } from "./Network/manager"
 import { CodeEditor } from "./components/Editor"
 
 
 function App() {
-    const nm = useRef<NetworkManager>(NetworkManager.getInstance()).current
     return (    
         <div>
             <h1>CodeJam</h1>
